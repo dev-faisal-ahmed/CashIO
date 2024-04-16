8da55456a9acaaca94586d7d06fa28920c70c4f4
Prashanta Chakraborty <prashanta0234@gmail.com>
2024-04-17 00:42:19 +0600

    added app router

---
+++ client/component/demoComponent.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const DemoComponent = () => {
+	return (
+		<View>
+			<Text>DemoComponent</Text>
+		</View>
+	);
+};
+
+export default DemoComponent;
