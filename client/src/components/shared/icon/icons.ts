6202783fd0dd246153a0dc9176750f20c6fbf2ea
dev-faisal-ahmed <ost.faisal.ahmed@gmail.com>
2024-04-22 03:47:02 +0600

    icons function created

---
+++ client/src/components/shared/icon/icons.ts
@@ -0,0 +1,24 @@
+export const icons = {
+  FontAwesome: {
+    bank: 'bank',
+  },
+
+  MaterialCommunityIcons: {
+    'bus-school': 'bus-school',
+  },
+
+  Ionicons: { school: 'school' },
+
+  Entypo: {
+    rocket: 'rocket',
+  },
+
+  MaterialIcons: {
+    'rocket-launch': 'rocket-launch',
+    school: 'school',
+  },
+
+  FontAwesome5: {
+    'car-side': 'car-side',
+  },
+};
