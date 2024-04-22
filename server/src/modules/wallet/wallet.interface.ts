c087bc505a16e3f682b3da95f0378c424a1eb7be
dev-faisal-ahmed <ost.faisal.ahmed@gmail.com>
2024-04-22 16:49:23 +0600

    api modified for create wallet

--- server/src/modules/wallet/wallet.interface.ts
+++ server/src/modules/wallet/wallet.interface.ts
@@ -1,11 +1,11 @@
 import { Schema } from 'mongoose';
+import { TIcon } from '../../utils/global-types';
 
 export type TWallet = {
   _id: Schema.Types.ObjectId;
   userId: Schema.Types.ObjectId;
   name: string;
-  income: number;
-  expense: number;
+  initialBalance: number;
   saving: boolean;
-  icon: string;
+  icon: TIcon;
 };
--- server/src/modules/wallet/wallet.validation.ts
+++ server/src/modules/wallet/wallet.validation.ts
@@ -2,10 +2,15 @@ import { z } from 'zod';
 
 const CreateWallet = z.object({
   name: z.string({ required_error: 'Wallet Name is required' }),
-  icon: z.string({ required_error: 'Wallet Icon is required' }),
+  icon: z.object(
+    {
+      group: z.string({ required_error: 'Icon Group is required' }),
+      name: z.string({ required_error: 'Icon Name is required' }),
+    },
+    { required_error: 'Icon is required' }
+  ),
   saving: z.boolean().optional(),
 });
 
 export const walletValidation = { CreateWallet };
-
 export type TCreateWalletPayload = z.infer<typeof CreateWallet>;
